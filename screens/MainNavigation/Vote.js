321c45ed5309d9e8f39738582dd9b28f8f87c072
Nitin thakkar <nitinthakkar@Nitins-MacBook-Air.local>
2017-11-12 18:52:03 +0530

    image picker working

--- screens/MainNavigation/Vote.js
+++ screens/MainNavigation/Vote.js
@@ -1,19 +1,56 @@
 import React from 'react';
 import checkAadharSubmit from '../../api/getUserDetail';
-import {View, Image,Text,StyleSheet} from 'react-native';
+import {View, Image,Text,StyleSheet,Alert} from 'react-native';
 import {Button,Header,Content,Container,ActionSheet} from 'native-base';
+import ImagePicker from 'react-native-image-picker';
 import store from '../../store';
-var BUTTONS = ["Option 0", "Option 1", "Option 2", "Delete", "Cancel"];
-var DESTRUCTIVE_INDEX = 3;
-var CANCEL_INDEX = 4;
+
+var options = {
+  title: 'Select Avatar',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+};
 class Vote extends React.Component {
    constructor(props){
      super(props);
      this.state={};
+     this.getImage.bind(this);
      var login=store.getState();
      checkAadharSubmit(login.login[0].email).then((res)=>{
        console.log("result :",res);
      });
+   }
+   getImage()
+   { /*
+     error is showing here
+     Undefined is not an object (evaluating 'ImagePickerManager.showImagePicker') on Android
+     */
+     ImagePicker.showImagePicker(options, (response) => {
+   console.log('Response = ', response);
+
+   if (response.didCancel) {
+     console.log('User cancelled image picker');
+   }
+   else if (response.error) {
+     console.log('ImagePicker Error: ', response.error);
+   }
+   else if (response.customButton) {
+     console.log('User tapped custom button: ', response.customButton);
+   }
+   else {
+     let source = { uri: response.uri };
+
+     // You can also display the image using data:
+     // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+     this.setState({
+       avatarSource: source
+     });
+   }
+ });
+
    }
 render(){
   return (
@@ -24,17 +61,7 @@ render(){
               style={{flex:1,height:200,width:null,margin:10}}
               source={{uri: 'http://ladasinghasan.weebly.com/uploads/8/0/3/8/8038657/aadhar_card_back.jpg'}}
             />
-          <Button onPress={()=> ActionSheet.show(
-              {
-                options: BUTTONS,
-                cancelButtonIndex: CANCEL_INDEX,
-                destructiveButtonIndex: DESTRUCTIVE_INDEX,
-                title: "Testing ActionSheet"
-              },
-              buttonIndex => {
-                this.setState({ clicked: BUTTONS[buttonIndex] });
-              }
-            )} block info>
+          <Button onPress={this.getImage} block info>
             <Text>Upload Aadhar</Text>
           </Button>
         </Content>
